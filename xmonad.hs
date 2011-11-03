8b1f2b6d7cac1e5f50f3fd389a33358bfe707c1f
Bernat Rafales <brafales@gnuine.com>
2011-11-03 14:58:05 +0100

    Some minor changes

--- xmonad.hs
+++ xmonad.hs
@@ -31,7 +31,6 @@ import XMonad.Layout.IM
 import XMonad.Layout.SimpleFloat
 import XMonad.Layout.Spacing
 import XMonad.Layout.ResizableTile
--- import XMonad.Layout.Gaps
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.Grid
@@ -53,7 +52,7 @@ modMask' = mod4Mask
 myWorkspaces    = ["1:main","2:web","3:vim","4:chat","5:music", "6:gimp"]
 -- Dzen/Conky
 myXmonadBar = "dzen2 -x '1440' -y '0' -h '24' -w '640' -ta 'l' -fg '#FFFFFF' -bg '#1B1D1E'"
-myStatusBar = "conky -c /home/brafales/.xmonad/.conky_dzen | dzen2 -x '2080' -w '1040' -h '24' -ta 'c' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'"
+myStatusBar = "conky -c /home/brafales/.xmonad/.conky_dzen | dzen2 -x '2080' -w '1040' -h '24' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'"
 myBitmapsDir = "/home/brafales/.xmonad/dzen2"
 --}}}
 -- Main {{{
@@ -70,7 +69,7 @@ main = do
       , logHook             = myLogHook dzenLeftBar >> fadeInactiveLogHook 0xdddddddd
       , normalBorderColor   = colorNormalBorder
       , focusedBorderColor  = colorFocusedBorder
-      , borderWidth   = 2
+      , borderWidth         = 2
 }
 --}}}
 
@@ -80,13 +79,12 @@ main = do
 manageHook' :: ManageHook
 manageHook' = (composeAll . concat $
     [ [resource     =? r            --> doIgnore            |   r   <- myIgnores] -- ignore desktop
-    , [className    =? c            --> doShift  "1:main"   |   c   <- myDev   ] -- move dev to main
+    , [className    =? c            --> doShift  "1:main"   |   c   <- myDev    ] -- move dev to main
     , [className    =? c            --> doShift  "2:web"    |   c   <- myWebs   ] -- move webs to main
-    , [className    =? c            --> doShift  "3:vim"    |   c   <- myVim   ] -- move webs to main
-    , [className    =? c            --> doShift	 "4:chat"   |   c   <- myChat  ] -- move chat to chat
-    , [className    =? c            --> doShift  "5:music"  |   c   <- myMusic   ] -- move music to music
-    , [className    =? c            --> doShift  "6:gimp"    |   c   <- myGimp   ] -- move img to div
---    , [name         =? "ncmpcpp"      --> doShift  "4:music"]
+    , [className    =? c            --> doShift  "3:vim"    |   c   <- myVim    ] -- move webs to main
+    , [className    =? c            --> doShift	 "4:chat"   |   c   <- myChat   ] -- move chat to chat
+    , [className    =? c            --> doShift  "5:music"  |   c   <- myMusic  ] -- move music to music
+    , [className    =? c            --> doShift  "6:gimp"   |   c   <- myGimp   ] -- move img to div
     , [className    =? c            --> doCenterFloat       |   c   <- myFloats ] -- float my floats
     , [name         =? n            --> doCenterFloat       |   n   <- myNames  ] -- float my names
     , [isFullscreen                 --> myDoFullFloat                           ]
@@ -101,11 +99,11 @@ manageHook' = (composeAll . concat $
         myFloats  = ["Smplayer","MPlayer","VirtualBox","Xmessage","XFontSel","Downloads","Nm-connection-editor"]
         myWebs    = ["Firefox","Google-chrome","Chromium", "Chromium-browser"]
         myMovie   = ["Boxee","Trine"]
-	myMusic	  = ["Rhythmbox","Spotify"]
-	myChat	  = ["Pidgin","Buddy List"]
-	myGimp	  = ["Gimp"]
-	myDev	  = ["gnome-terminal"]
-	myVim	  = ["Gvim"]
+        myMusic	  = ["Rhythmbox","Spotify"]
+        myChat	  = ["Pidgin","Buddy List"]
+        myGimp	  = ["Gimp"]
+        myDev	  = ["gnome-terminal"]
+        myVim	  = ["Gvim"]
 
         -- resources
         myIgnores = ["desktop","desktop_window","notify-osd","stalonetray","trayer"]
@@ -117,7 +115,6 @@ manageHook' = (composeAll . concat $
 myDoFullFloat :: ManageHook
 myDoFullFloat = doF W.focusDown <+> doFullFloat
 -- }}}
--- layoutHook' = customLayout
 layoutHook'  =  onWorkspaces ["1:main","5:music"] customLayout $ onWorkspaces ["6:gimp"] gimpLayout $ onWorkspaces ["4:chat"] imLayout $
                 customLayout2
 
@@ -145,56 +142,31 @@ myLogHook h = dynamicLogWithPP $ defaultPP
     }
 
 -- Layout
-customLayout = avoidStruts $ smartBorders tiled ||| smartBorders (Mirror tiled) ||| noBorders Full ||| smartBorders simpleFloat
+customLayout = avoidStruts $ tiled ||| Mirror tiled ||| Full ||| simpleFloat
   where
-    --tiled = ResizableTall 1 (2/100) (1/2) []
-    tiled   = spacing pxl $ ResizableTall 1 (2/100) (1/2) []
---    nmaster = 1   
---    delta   = 2/100
---    ratio   = 1/2
-    pxl     = 5
+    tiled   = ResizableTall 1 (2/100) (1/2)
 
-customLayout2 = avoidStruts $ noBorders Full ||| smartBorders tiled ||| smartBorders (Mirror tiled) ||| smartBorders simpleFloat
+customLayout2 = avoidStruts $ Full ||| tiled ||| Mirror tiled ||| simpleFloat
   where
-    --tiled = ResizableTall 1 (2/100) (1/2) []
-    tiled   = spacing pxl $ ResizableTall 1 (2/100) (1/2) []
---    nmaster = 1   
---    delta   = 2/100
---    ratio   = 1/2
-    pxl     = 5
+    tiled   = ResizableTall 1 (2/100) (1/2)
 
 gimpLayout  = avoidStruts $ withIM (0.11) (Role "gimp-toolbox") $
               reflectHoriz $
               withIM (0.15) (Role "gimp-dock") Full
 
 imLayout    = avoidStruts $ withIM (1%5) (And (ClassName "Pidgin") (Role "buddy_list")) Grid 
---  where
---    tiled   = spacing pxl $ ResizableTall 1 (2/100) (1/2) []
---    nmaster = 1
---    delta   = 2/100
---    ratio   = 1/2
---    pxl     = 5
 --}}}
 -- Theme {{{
 -- Color names are easier to remember:
---colorOrange          = "#ff7701"
 colorOrange         = "#FD971F"
---colorDarkGray        = "#171717"
 colorDarkGray       = "#1B1D1E"
---colorPink            = "#e3008d"
 colorPink           = "#F92672"
---colorGreen           = "#00aa4a"
 colorGreen          = "#A6E22E"
---colorBlue            = "#008dd5"
 colorBlue           = "#66D9EF"
---colorYellow          = "#fee100"
 colorYellow         = "#E6DB74"
---colorWhite           = "#cfbfad"
 colorWhite          = "#CCCCC6"
  
---colorNormalBorder    = "#1c2636"
 colorNormalBorder   = "#CCCCC6"
---colorFocusedBorder   = "#ebac54"
 colorFocusedBorder  = "#fd971f"
 
 
@@ -235,27 +207,27 @@ keys' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask,		            xK_o        ), spawn "chromium-browser")
     , ((modMask,                    xK_m        ), spawn "nautilus --no-desktop --browser")
     -- Media Keys
-    , ((0,                          0x1008ff12  ), spawn "amixer -q sset Master toggle") -- XF86AudioMute
-    , ((0,                          0x1008ff11  ), spawn "amixer -q sset Master 655- unmute") -- XF86AudioLowerVolume
-    , ((0,                          0x1008ff13  ), spawn "amixer -q sset Master 655+ unmute") -- XF86AudioRaiseVolume
+    , ((0,                          0x1008ff12  ), spawn "amixer -q sset Master toggle")        -- XF86AudioMute
+    , ((0,                          0x1008ff11  ), spawn "amixer -q sset Master 655- unmute")   -- XF86AudioLowerVolume
+    , ((0,                          0x1008ff13  ), spawn "amixer -q sset Master 655+ unmute")   -- XF86AudioRaiseVolume
     , ((0,                          0x1008ff14  ), spawn "rhythmbox-client --play-pause")
     , ((0,                          0x1008ff17  ), spawn "rhythmbox-client --next")
     , ((0,                          0x1008ff16  ), spawn "rhythmbox-client --previous")
 
     -- layouts
     , ((modMask,                    xK_space    ), sendMessage NextLayout)
-    , ((modMask .|. shiftMask,      xK_space    ), setLayout $ XMonad.layoutHook conf) -- reset layout on current desktop to default
+    , ((modMask .|. shiftMask,      xK_space    ), setLayout $ XMonad.layoutHook conf)          -- reset layout on current desktop to default
     , ((modMask,                    xK_b        ), sendMessage ToggleStruts)
     , ((modMask,                    xK_n        ), refresh)
-    , ((modMask,                    xK_Tab      ), windows W.focusDown) -- move focus to next window
+    , ((modMask,                    xK_Tab      ), windows W.focusDown)                         -- move focus to next window
     , ((modMask,                    xK_j        ), windows W.focusDown)
     , ((modMask,                    xK_k        ), windows W.focusUp  )
-    , ((modMask .|. shiftMask,      xK_j        ), windows W.swapDown) -- swap the focused window with the next window
-    , ((modMask .|. shiftMask,      xK_k        ), windows W.swapUp)  -- swap the focused window with the previous window
+    , ((modMask .|. shiftMask,      xK_j        ), windows W.swapDown)                          -- swap the focused window with the next window
+    , ((modMask .|. shiftMask,      xK_k        ), windows W.swapUp)                            -- swap the focused window with the previous window
     , ((modMask,                    xK_Return   ), windows W.swapMaster)
-    , ((modMask,                    xK_t        ), withFocused $ windows . W.sink) -- Push window back into tiling
-    , ((modMask,                    xK_h        ), sendMessage Shrink) -- %! Shrink a master area
-    , ((modMask,                    xK_l        ), sendMessage Expand) -- %! Expand a master area
+    , ((modMask,                    xK_t        ), withFocused $ windows . W.sink)              -- Push window back into tiling
+    , ((modMask,                    xK_h        ), sendMessage Shrink)                          -- %! Shrink a master area
+    , ((modMask,                    xK_l        ), sendMessage Expand)                          -- %! Expand a master area
     , ((modMask,                    xK_comma    ), sendMessage (IncMasterN 1))
     , ((modMask,                    xK_period   ), sendMessage (IncMasterN (-1)))
 
